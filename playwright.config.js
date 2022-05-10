8e6aa3d108fbf61ac41e085496662a3dc34cc504
Nikhil Bisht <nikhilbisht96@gmail.com>
2022-05-10 21:21:21 +0530

    added config to run test in other browsers

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,22 @@
+const { devices } = require('@playwright/test');
+
 module.exports = {
 
     // Each test is given 30 seconds
     timeout : 1300000,
-    reporter : [ ['html', { open: 'never' }] ]
+    reporter : [ ['html', { open: 'never' }] ],
+      projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Edge'] },
+    },
+  ],
   };
